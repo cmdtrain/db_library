dceaf865411cb3aa7e8cf01cc156470c32d56b78
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2025-05-31 17:22:57 +0300

    Update db design- library project-1748290018.sql
    hw 10 - update

--- db design- library project-1748290018.sql
+++ db design- library project-1748290018.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "members" (
 	"memberID" serial NOT NULL UNIQUE,
 	"name" varchar(255) NOT NULL,
 	"contactInfo" bigint NOT NULL,
+	"joinDate" DATE NOT NULL DEFAULT CURRENT_DATE,
 	PRIMARY KEY ("memberID")
 );
 
@@ -61,116 +62,130 @@ CREATE TABLE IF NOT EXISTS "readingHistory" (
 
 
 
-
 -- завд №9
 -- DML Postgresql
 -- 7 таблиць
 -- включно з INSERT, UPDATE, DELETE
 
+-- fix сумісності "ISBN"
+TRUNCATE TABLE 
+  "members", 
+  "books", 
+  "bookCopies", 
+  "borrowings", 
+  "reservations", 
+  "reviews", 
+  "readingHistory" 
+RESTART IDENTITY CASCADE;
 
 -- 1. Members
+-- fix - joinDate для Mixed View не працював
+ALTER TABLE "members"
+DROP COLUMN IF EXISTS "joinDate";
 
-INSERT INTO "members" ("memberID", "name", "contactInfo") VALUES
-(1, 'Alice Cooper', 9123456789),
-(2, 'John Smith', 9998887766),
-(3, 'Joe Biden', 9234567890),
-(4, 'Donald Trump', 9111222333),
-(5, 'Volodymyr Zelensky', 9334445566),
-(6, 'Kyrylo Budanov', 9445556677),
-(7, 'Emma Watson', 9101112233),
-(8, 'Daniel Craig', 9224446688),
-(9, 'Grace Park', 9335557799),
-(10, 'Liam Neeson', 9556667788);
+ALTER TABLE "members"
+ADD COLUMN "joinDate" DATE NOT NULL DEFAULT CURRENT_DATE;
+INSERT INTO "members" ("name", "contactInfo", "joinDate") VALUES
+('Alice Cooper', 9123456789, '2012-08-02'),
+('John Smith', 9998887766, '2001-01-14'),
+('Joe Biden', 9234567890, '2016-09-07'),
+('Donald Trump', 9111222333, '1999-10-30'),
+('Volodymyr Zelensky', 9334445566, '2020-03-22'),
+('Kyrylo Budanov', 9445556677, '2024-11-10'),
+('Emma Watson', 9101112233, '2002-12-01'),
+('Daniel Craig', 9224446688, '1997-04-27'),
+('Grace Park', 9335557799, '2009-07-03'),
+('Liam Neeson', 9556667788, '2019-08-08');
 
 
 -- 2. Books
 
-INSERT INTO "books" ("bookId", "title", "authors", "ISBN", "publicationYear") VALUES
-(1, 'The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 1925),
-(2, 'To Kill a Mockingbird', 'Harper Lee', '9780061120084', 1960),
-(3, '1984', 'George Orwell', '9780451524935', 1949),
-(4, 'The Hobbit', 'J.R.R. Tolkien', '9780547928227', 1937),
-(5, 'Pride and Prejudice', 'Jane Austen', '9780141439518', 1813),
-(6, 'The Catcher in the Rye', 'J.D. Salinger', '9780316769488', 1951),
-(7, 'The Alchemist', 'Paulo Coelho', '9780062315007', 1988),
-(8, 'A Brief History of Time', 'Stephen Hawking', '9780553380163', 1988),
-(9, 'The Da Vinci Code', 'Dan Brown', '9780307474278', 2003),
-(10, 'The Road', 'Cormac McCarthy', '9780307387899', 2006);
+INSERT INTO "books" ("title", "authors", "ISBN", "publicationYear") VALUES
+('The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 1925),
+('To Kill a Mockingbird', 'Harper Lee', '9780061120084', 1960),
+('1984', 'George Orwell', '9780451524935', 1949),
+('The Hobbit', 'J.R.R. Tolkien', '9780547928227', 1937),
+('Pride and Prejudice', 'Jane Austen', '9780141439518', 1813),
+('The Catcher in the Rye', 'J.D. Salinger', '9780316769488', 1951),
+('The Alchemist', 'Paulo Coelho', '9780062315007', 1988),
+('A Brief History of Time', 'Stephen Hawking', '9780553380163', 1988),
+('The Da Vinci Code', 'Dan Brown', '9780307474278', 2003),
+('The Road', 'Cormac McCarthy', '9780307387899', 2006);
 
 
 -- 3. BookCopies
 
-INSERT INTO "bookCopies" ("copyID", "bookID", "copyStatus") VALUES
-(1, 1, 'Available'),
-(2, 1, 'Borrowed'),
-(3, 2, 'Available'),
-(4, 3, 'Available'),
-(5, 4, 'Reserved'),
-(6, 5, 'Available'),
-(7, 6, 'Borrowed'),
-(8, 7, 'Available'),
-(9, 8, 'Available'),
-(10, 9, 'Available');
+INSERT INTO "bookCopies" ("bookID", "copyStatus") VALUES
+(1, 'Available'),
+(1, 'Borrowed'),
+(2, 'Available'),
+(3, 'Available'),
+(4, 'Reserved'),
+(5, 'Available'),
+(6, 'Borrowed'),
+(7, 'Available'),
+(8, 'Available'),
+(9, 'Available');
 
 
 -- 4. Borrowings
 
-INSERT INTO "borrowings" ("borrowingID", "memberID", "bookID", "borrowingDate", "dueDate", "returnDate") VALUES
-(1, 1, 2, '2024-03-01', '2024-03-15', '2024-03-14'),
-(2, 3, 5, '2024-03-05', '2024-03-19', NULL),
-(3, 4, 1, '2024-03-10', '2024-03-24', '2024-03-23'),
-(4, 6, 3, '2024-03-12', '2024-03-26', NULL),
-(5, 2, 4, '2024-03-15', '2024-03-29', '2024-03-28'),
-(6, 7, 7, '2024-03-17', '2024-03-31', NULL),
-(7, 5, 6, '2024-03-20', '2024-04-03', '2024-04-01'),
-(8, 9, 8, '2024-03-22', '2024-04-05', NULL),
-(9, 10, 9, '2024-03-25', '2024-04-08', NULL),
-(10, 8, 10, '2024-03-27', '2024-04-10', '2024-04-09');
+INSERT INTO "borrowings" ("memberID", "bookID", "borrowingDate", "dueDate", "returnDate") VALUES
+(1, 2, '2024-03-01', '2024-03-15', '2024-03-14'),
+(3, 5, '2024-03-05', '2024-03-19', NULL),
+(4, 1, '2024-03-10', '2024-03-24', '2024-03-23'),
+(6, 3, '2024-03-12', '2024-03-26', NULL),
+(2, 4, '2024-03-15', '2024-03-29', '2024-03-28'),
+(7, 7, '2024-03-17', '2024-03-31', NULL),
+(5, 6, '2024-03-20', '2024-04-03', '2024-04-01'),
+(9, 8, '2024-03-22', '2024-04-05', NULL),
+(10, 9, '2024-03-25', '2024-04-08', NULL),
+(8, 10, '2024-03-27', '2024-04-10', '2024-04-09');
 
 
 -- 5. Reservations
 
-INSERT INTO "reservations" ("reservationID", "memberID", "bookID", "reservationDate", "status") VALUES
-(1, 2, 3, '2024-04-01', 'Active'),
-(2, 4, 2, '2024-04-02', 'Active'),
-(3, 5, 4, '2024-04-03', 'Cancelled'),
-(4, 6, 1, '2024-04-04', 'Active'),
-(5, 7, 5, '2024-04-05', 'Active'),
-(6, 8, 6, '2024-04-06', 'Active'),
-(7, 9, 7, '2024-04-07', 'Active'),
-(8, 1, 8, '2024-04-08', 'Active'),
-(9, 3, 9, '2024-04-09', 'Active'),
-(10, 10, 10, '2024-04-10', 'Expired');
+INSERT INTO "reservations" ("memberID", "bookID", "reservationDate", "status") VALUES
+(2, 3, '2024-04-01', 'Active'),
+(4, 2, '2024-04-02', 'Active'),
+(5, 4, '2024-04-03', 'Cancelled'),
+(6, 1, '2024-04-04', 'Active'),
+(7, 5, '2024-04-05', 'Active'),
+(8, 6, '2024-04-06', 'Active'),
+(9, 7, '2024-04-07', 'Active'),
+(1, 8, '2024-04-08', 'Active'),
+(3, 9, '2024-04-09', 'Active'),
+(10, 10, '2024-04-10', 'Expired');
 
 
 -- 6. Reviews
 
-INSERT INTO "reviews" ("reviewID", "memberID", "bookID", "reviewText", "rating") VALUES
-(1, 1, 1, 'A classic masterpiece.', 5),
-(2, 2, 2, 'Very insightful and emotional.', 4),
-(3, 3, 3, 'Still relevant today.', 5),
-(4, 4, 4, 'Loved the fantasy elements.', 4),
-(5, 5, 5, 'Well-written but not my taste.', 3),
-(6, 6, 6, 'A compelling story.', 5),
-(7, 7, 7, 'Very motivating.', 4),
-(8, 8, 8, 'Challenging but worth it.', 4),
-(9, 9, 9, 'A thrilling read!', 5),
-(10, 10, 10, 'Dark and gripping.', 4);
+INSERT INTO "reviews" ("memberID", "bookID", "reviewText", "rating") VALUES
+(1, 1, 'A classic masterpiece.', 5),
+(2, 2, 'Very insightful and emotional.', 4),
+(3, 3, 'Still relevant today.', 5),
+(4, 4, 'Loved the fantasy elements.', 4),
+(5, 5, 'Well-written but not my taste.', 3),
+(6, 6, 'A compelling story.', 5),
+(7, 7, 'Very motivating.', 4),
+(8, 8, 'Challenging but worth it.', 4),
+(9, 9, 'A thrilling read!', 5),
+(10, 10, 'Dark and gripping.', 4);
 
 
 -- 7. ReadingHistory
 
-INSERT INTO "readingHistory" ("historyID", "memberID", "bookID", "startDate", "endDate") VALUES
-(1, 1, 1, '2024-02-01', '2024-02-15'),
-(2, 2, 2, '2024-02-10', '2024-02-20'),
-(3, 3, 3, '2024-02-12', '2024-02-25'),
-(4, 4, 4, '2024-03-01', '2024-03-10'),
-(5, 5, 5, '2024-03-05', '2024-03-15'),
-(6, 6, 6, '2024-03-10', '2024-03-20'),
-(7, 7, 7, '2024-03-15', NULL),
-(8, 8, 8, '2024-03-20', '2024-03-30'),
-(9, 9, 9, '2024-03-25', NULL),
-(10, 10, 10, '2024-04-01', '2024-04-10');
+INSERT INTO "readingHistory" ("memberID", "bookID", "startDate", "endDate") VALUES
+(1, 1, '2024-02-01', '2024-02-15'),
+(2, 2, '2024-02-10', '2024-02-20'),
+(3, 3, '2024-02-12', '2024-02-25'),
+(4, 4, '2024-03-01', '2024-03-10'),
+(5, 5, '2024-03-05', '2024-03-15'),
+(6, 6, '2024-03-10', '2024-03-20'),
+(7, 7, '2024-03-15', NULL),
+(8, 8, '2024-03-20', '2024-03-30'),
+(9, 9, '2024-03-25', NULL),
+(10, 10, '2024-04-01', '2024-04-10');
 
 
 
@@ -187,3 +202,75 @@ WHERE "reservationID" = 10;
 -- пояснення:
 -- без DELETE для табличок: 1-Members, 2-Books, 6-Reviews, 4-Borrowings, 7-ReadingHistory - для збереження цілісності бази даних
 -- без UPDATE для табличок: 2-Books, 4-Borrowings, 7-ReadingHistory - апдейт може поламати бд
+
+
+
+
+--завд №10
+--views
+
+-- 1 Horizontal
+CREATE OR REPLACE VIEW view_book_titles_authors AS
+SELECT "title", "authors"
+FROM "books";
+
+-- 2 Vertical
+CREATE OR REPLACE VIEW view_overdue_borrowings AS
+SELECT *
+FROM "borrowings"
+WHERE "returnDate" IS NULL AND "dueDate" < CURRENT_DATE;
+
+-- 3 Mixed
+CREATE OR REPLACE VIEW view_active_members_contact AS
+SELECT "memberID", "name", "contactInfo"
+FROM "members"
+WHERE "joinDate" >= CURRENT_DATE - INTERVAL '3 months';
+
+
+-- 4 JOIN
+CREATE OR REPLACE VIEW view_members_borrowed_books AS
+SELECT 
+    m."memberID",
+    m."name" AS member_name,
+    b."title" AS book_title,
+    br."borrowingDate",
+    br."dueDate",
+    br."returnDate"
+FROM "members" m
+JOIN "borrowings" br ON m."memberID" = br."memberID"
+JOIN "books" b ON b."bookId" = br."bookID";
+
+
+-- 5 Subquery
+CREATE OR REPLACE VIEW view_top_rated_books AS
+SELECT b."bookId", b."title", b."authors", avg_ratings.avg_rating
+FROM "books" b
+JOIN (
+    SELECT "bookID", AVG("rating") AS avg_rating
+    FROM "reviews"
+    GROUP BY "bookID"
+) avg_ratings ON b."bookId" = avg_ratings."bookID"
+WHERE avg_ratings.avg_rating > 4;
+
+
+-- 6 UNION
+CREATE OR REPLACE VIEW view_all_reservations_union AS
+SELECT "reservationID", "memberID", "bookID", "reservationDate", "status"
+FROM "reservations"
+WHERE "status" IN ('Pending', 'Reserved')
+UNION
+SELECT "reservationID", "memberID", "bookID", "reservationDate", "status"
+FROM "reservations"
+WHERE "status" = 'Expired';
+
+-- 7 View from another view
+CREATE OR REPLACE VIEW view_names_overdue_borrowers AS
+SELECT DISTINCT m."name"
+FROM view_overdue_borrowings ob
+JOIN "members" m ON ob."memberID" = m."memberID";
+
+-- 8 CHECK OPTION
+CREATE OR REPLACE VIEW view_available_books WITH (security_barrier) AS
+SELECT * FROM "bookCopies"
+WHERE "copyStatus" = 'Available'
+WITH CHECK OPTION;
